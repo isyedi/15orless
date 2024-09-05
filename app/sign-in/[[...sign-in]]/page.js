04bda115cbe9c7f6531959754d1d98c4b233500d
Brandon Fernando <brandofernando.3@gmail.com>
2024-09-05 15:46:09 -0400

    mobile friendly lp and signin

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useRef } from 'react'
 import { Box } from '@mui/material'
 import { SignIn } from '@clerk/nextjs'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
@@ -11,9 +12,8 @@ const alfaSlabOne = Alfa_Slab_One({
   display: 'swap',
 });
 
-
-
 export default function SignInPage() {
+
     return (
         <>
         <Head>
@@ -24,31 +24,39 @@ export default function SignInPage() {
             width="100vw"
             height="100vh"
             display="flex"
+            alignItems="center"
+            justifyContent="center"
             sx={{
                 backgroundImage: "url('/bg-image.png')",
                 backgroundSize: "cover", 
                 backgroundPosition: "center", 
                 backgroundRepeat: "no-repeat", 
+                padding: 2,
             }}
         >
             
             <Box
-                ml="50%"
-                width="50%"
-                height="100%"
+                width={{ xs: '90%', sm: '80%', md: '60%', lg: '50%' }}
+                maxWidth="600px"  
+                height="auto"
                 bgcolor="white"
                 display="flex"
                 flexDirection="column"
                 justifyContent="center"
                 alignItems="center"
+                border="3px solid black"
                 sx={{
+                    boxShadow: '10px 10px 0px 0px rgba(0, 0, 0, 1)',
+                    p: { xs: 2, sm: 3 },
+                    position: 'relative',
                     '.cl-internal-1dauvpw': {
-                      display: 'none',
-                    },
-                  }}
+                        display: 'none', 
+                      },
+                      
+                }}
             >
-                <a href="/" style={{ textDecoration: 'none', cursor: 'pointer', marginBottom: '16px'  }}>
-                    <ArrowBackIcon />
+                <a href="/" style={{ textDecoration: 'none', cursor: 'pointer', marginBottom: '16px' }}>
+                    <ArrowBackIcon style={{ fontSize: '24px' }} />
                 </a>
 
                 <SignIn
@@ -56,7 +64,8 @@ export default function SignInPage() {
                     appearance={{
                         elements: {
                             formButtonPrimary: {
-                                fontSize: 24,
+                                fontSize: { xs: '16px', sm: '18px' },
+                                padding: { xs: '10px', sm: '12px' },
                             },
                             cardBox:{
                                 border: "none",
@@ -68,8 +77,17 @@ export default function SignInPage() {
                                 boxShadow: "none"
                             }, 
                             headerTitle:{
-                                fontSize: 40,
+                                fontSize: { xs: '24px', sm: '30px' },
                             },
+                            footerActionText:{
+                                fontSize: 13
+                            },
+                            footerActionLink:{
+                                fontSize: 13
+                            },
+                            socialButtonsBlockButtonText:{
+                                fontSize: 11
+                            }
                             
                         },
                         variables: {
